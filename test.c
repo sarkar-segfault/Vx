cf10f194e6da478d0ea587ca55a8acc9ee47f5ec
Adhrit Sarkar <adhritsarkar@proton.me>
2026-04-03 13:44:26 +0530

    ignored compile_flags.txt, and edited sources/cmake

--- test.c
+++ test.c
@@ -14,36 +14,6 @@ int main(void) {
     VxWindow_Update(window);
   }
 
-  // int w, h;
-  // VxWindow_GetSize(window, &w, &h);
-  // printf("w: %d, h: %d\n", w, h);
-
-  // int x, y;
-  // VxWindow_GetPos(window, &x, &y);
-  // printf("x: %d, y: %d\n", x, y);
-
-  // VxWindow_Delete(window);
-
-  // DWORD err = GetLastError();
-  // char *msg = NULL;
-
-  // FormatMessage(
-  //   FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-  //   NULL,
-  //   err,
-  //   0,
-  //   (LPSTR)&msg,
-  //   0,
-  //   NULL
-  // );
-
-  // if (msg) {
-  //   printf("Win32 API Error #%lu: %s", err, msg);
-  //   LocalFree(msg);
-  // } else {
-  //   printf("Win32 API Error #%lu", err);
-  // }
-
   Vx_Terminate();
   return 0;
 }
