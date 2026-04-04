cc5361457beb211640ba1eb8cf023bdc12e29692
Adhrit Sarkar <adhritsarkar@proton.me>
2026-04-04 21:35:05 +0530

    Ring -> EventRing

---
+++ Headers/Vx/EventRing.h
@@ -0,0 +1,45 @@
+#ifndef Vx__EventRingH
+#define Vx__EventRingH
+
+#include "Event.h"
+#include "_Expose.h"
+#include <stdbool.h>
+#include <stddef.h>
+
+#ifndef VxEventRing_Length
+  #define VxEventRing_Length 64
+#endif
+
+/**
+  @since 0.1.0
+  @brief A ring buffer of `VxEventRing_Length` `VxEvent`s, that wraps around when filled.
+*/
+typedef struct VxEventRing VxEventRing;
+
+/**
+  @since 0.1.0
+  @brief Push an event onto the event ring.
+  @returns Whether pushing was successful.
+*/
+Vx__Expose bool VxEventRing_Push(VxEventRing *er, const VxEvent event);
+
+/**
+  @since 0.1.0
+  @brief Pop the last event from the event ring.
+  @returns Whether popping was successful.
+*/
+Vx__Expose bool VxEventRing_Pop(VxEventRing *er, const VxEvent *event);
+
+/**
+  @since 0.1.0
+  @returns The latest event from the event ring.
+*/
+Vx__Expose bool VxEventRing_Head(VxEventRing *er, const size_t **head);
+
+/**
+  @since 0.1.0
+  @returns The last event from the event ring.
+*/
+Vx__Expose bool VxEventRing_Tail(VxEventRing *er, const size_t **tail);
+
+#endif
