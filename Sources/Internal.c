2bde7382c480019bdfec85932c2103812b1c60d9
Adhrit Sarkar <adhritsarkar@proton.me>
2026-04-08 12:47:08 +0530

    removed all traces of VxEventRing, rip

--- Sources/Internal.c
+++ Sources/Internal.c
@@ -3,36 +3,6 @@
 #include <stdbool.h>
 #include <Windows.h>
 
-#define VxEventRing__Wrap(var) var = (var + 1) % VxEventRing_Length
-
-bool VxEventRing_Put(VxEventRing *ring, VxEvent event) {
-  if (!ring) return false;
-
-  ring->events[ring->head] = event;
-  VxEventRing__Wrap(ring->head);
-
-  if (ring->full) {
-    VxEventRing__Wrap(ring->tail);
-  }
-
-  ring->full = ring->head == ring->tail;
-  return true;
-}
-
-bool VxEventRing_Pop(VxEventRing *ring, VxEvent *event) {
-  if (
-    !ring ||
-    !event ||
-    (!ring->full && ring->head == ring->tail)
-  ) return false;
-
-  *event = ring->events[ring->tail];
-  VxEventRing__Wrap(ring->tail);
-
-  ring->full = false;
-  return true;
-}
-
 bool Vx__TranslateEvent(const MSG *msg, VxEvent *event) {
   if (!msg || !event) return false;
   *event = (VxEvent){0};
