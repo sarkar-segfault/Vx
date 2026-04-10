a7f43755620251d44e774f8acc81fcbc787c39e8
Adhrit Sarkar <adhritsarkar@proton.me>
2026-04-10 23:17:59 +0530

    sanitized Internal.c to remove -Wabsolute-value

--- Sources/Internal.c
+++ Sources/Internal.c
@@ -7,22 +7,24 @@
 #include "Vx/Event.h"
 
 #define VxEventRing__Wrap(var) var = (var + 1) % VxEventRing_Length
-#define Vx__Near(latest, event, a, b) abs(latest->info.a - event.info.a) <= 15 && abs(latest->info.b - event.info.b) <= 15
+
+#define Vx__INear(latest, event, a, b) abs(latest->info.a - event.info.a) <= 16 && abs(latest->info.b - event.info.b) <= 16
+#define Vx__UNear(latest, event, a, b) latest->info.a - event.info.a <= 16 && latest->info.b - event.info.b <= 16
 
 bool VxEventRing_Put(VxEventRing *ring, VxEvent event) {
   if (!ring) return false;
 
   VxEvent *latest = &ring->events[ring->head - 1];
 
-  if (latest->type == VxEventType_Move && latest->type == event.type && Vx__Near(latest, event, pos.x, pos.y)) {
+  if (latest->type == VxEventType_Move && latest->type == event.type && Vx__INear(latest, event, pos.x, pos.y)) {
     latest->info.pos.x = event.info.pos.x;
     latest->info.pos.y = event.info.pos.y;
     return true;
-  } else if (latest->type == VxEventType_Resize && latest->type == event.type && Vx__Near(latest, event, size.w, size.h)) {
+  } else if (latest->type == VxEventType_Resize && latest->type == event.type && Vx__UNear(latest, event, size.w, size.h)) {
     latest->info.size.w = event.info.size.w;
     latest->info.size.h = event.info.size.h;
     return true;
-  } else if (latest->type == VxEventType_MouseMove && latest->type == event.type && Vx__Near(latest, event, pos.x, pos.y)) {
+  } else if (latest->type == VxEventType_MouseMove && latest->type == event.type && Vx__UNear(latest, event, pos.x, pos.y)) {
     latest->info.pos.x = event.info.pos.x;
     latest->info.pos.y = event.info.pos.y;
     return true;
