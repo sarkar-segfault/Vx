ace3077c23a04e47305807dd14d2249bfaea8131
Adhrit Sarkar <adhritsarkar@proton.me>
2026-04-11 14:37:43 +0530

    documented Context.h

--- Headers/Vx/Context.h
+++ Headers/Vx/Context.h
@@ -12,10 +12,28 @@
 
 #include "_Expose.h"
 
+/*
+  `VxContext` - wrapper for storing information required for windowing to work.
+
+  This struct is a wrapper for storing extra information we need to make windowing
+  and graphics work. Internally, this stores a `EGLDisplay` and a `EGLConfig`.
+*/
 typedef struct VxContext *VxContext;
 
-Vx__Expose bool VxContext_Initiate(VxContext *device);
+/*
+  `VxContext_Initiate` - initiate windowing and graphics.
 
-Vx__Expose bool VxContext_Terminate(VxContext device);
+  This function uses platform APIs to allocate the required resources for windowing
+  and graphics. It creates a EGL display and a config, writing it into the context.
+*/
+Vx__Expose bool VxContext_Initiate(VxContext *context);
+
+/*
+  `VxContext_Terminate` - terminate windowing and graphics.
+
+  This function uses platform APIs to deallocate the resources we `VxContext_Initiate`
+  allocated for windowing and graphics. It frees all EGL and windowing contexts.
+*/
+Vx__Expose bool VxContext_Terminate(VxContext context);
 
 #endif
