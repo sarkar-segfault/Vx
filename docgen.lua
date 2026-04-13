c07422a3f9b525c9a0882e95d24659134e840cbb
Adhrit Sarkar <adhritsarkar@proton.me>
2026-04-13 23:55:19 +0530

    added lua glue script to gen docs

---
+++ docgen.lua
@@ -0,0 +1,24 @@
+#!/usr/bin/env lua
+
+local function emit(file)
+  local hndl = io.open("Headers/Vx/" .. file .. ".h", "r")
+  assert(hndl, "failed to read file")
+
+  local text = hndl:read("*a")
+  hndl:close()
+
+  local out = io.open("docs/" .. file .. ".md", "w")
+  assert(out, "failed to open out file")
+
+  for comment in string.gmatch(text, "/%*%*(.-)%*%*/") do
+    out:write(comment)
+  end
+
+  out:close()
+end
+
+os.execute("mkdir docs")
+emit("Vx")
+emit("Event")
+emit("Window")
+emit("Context")
