32c9a89aa1508c3d5c8f300ca72fb884a0e5f253
Adhrit Sarkar <adhritsarkar@proton.me>
2026-04-14 13:39:13 +0530

    removed readme work from pages.yml to docgen.lua

--- docgen.lua
+++ docgen.lua
@@ -10,15 +10,22 @@ local function emit(file)
   local out = io.open("docs/" .. file .. ".md", "w")
   assert(out, "failed to open out file")
 
-  out:write("---\nlayout: page\n---\n")
+  out:write("---\nlayout: default\n---\n")
 
   for comment in string.gmatch(text, "/%*%*(.-)%*%*/") do
-    local result, _ = comment:gsub("  ", "");
+    local _, result = comment:gsub("  ", "")
     out:write(result)
   end
 
   out:close()
 end
+
+local index = io.open("docs/index.md", "w")
+local readme = io.open("README.md", "r")
+assert(index and readme, "failed to open files")
+
+local result, _ = readme:read("*a"):gsub("^[^\n]*\n")
+index:write(result)
 
 emit("Vx")
 emit("Event")
