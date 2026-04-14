4ca11f18add53dda56cba7ab30d3b2c2819b0df5
Adhrit Sarkar <adhritsarkar@proton.me>
2026-04-14 13:48:08 +0530

    changed layout for docs to `page` from `default`

--- docgen.lua
+++ docgen.lua
@@ -10,7 +10,7 @@ local function emit(file)
   local out = io.open("docs/" .. file .. ".md", "w")
   assert(out, "failed to open out file")
 
-  out:write("---\nlayout: default\n---\n")
+  out:write("---\nlayout: page\n---\n")
 
   for comment in string.gmatch(text, "/%*%*(.-)%*%*/") do
     local result, _ = comment:gsub("  ", "")
