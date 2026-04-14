60339d6bbfbd0665e9efd0e2ad0d0357a68c9456
Adhrit Sarkar <adhritsarkar@proton.me>
2026-04-14 13:21:30 +0530

    changed doc generation

--- docgen.lua
+++ docgen.lua
@@ -10,6 +10,8 @@ local function emit(file)
   local out = io.open("docs/" .. file .. ".md", "w")
   assert(out, "failed to open out file")
 
+  out:write("---\nlayout: page\n---\n")
+
   for comment in string.gmatch(text, "/%*%*(.-)%*%*/") do
     out:write(comment)
   end
---
+++ docs/files.md
@@ -0,0 +1,10 @@
+---
+layout: page
+title: Files
+---
+
+Vx has a relatively short number of header files. The documentation for public ones is listed below:
+1. [Vx.h](/Vx.md)
+2. [Event.h](/Event.md)
+3. [Window.h](/Window.md)
+4. [Context.h](/Context.md)
