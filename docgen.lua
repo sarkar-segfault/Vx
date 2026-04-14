e6d382e79f0ddb9db5729a137fce707861c41c1b
Adhrit Sarkar <adhritsarkar@proton.me>
2026-04-14 14:50:54 +0530

    tweaked doc generation

--- docgen.lua
+++ docgen.lua
@@ -10,9 +10,16 @@ local function emit(file)
   local out = io.open("docs/" .. file .. ".md", "w")
   assert(out, "failed to open out file")
 
-  out:write("---\nlayout: base\n---\n")
+  out:write("---\nlayout: page\ntitle: ")
 
+  local first = true
   for comment in string.gmatch(text, "/%*%*(.-)%*%*/") do
+    if first then
+      first = false
+      local result, _ = text:match("([^\n]*)")
+      out:write(result .. "\n---")
+    end
+
     local result, _ = comment:gsub("  ", "")
     out:write(result)
   end
@@ -24,8 +31,9 @@ local index = io.open("docs/index.md", "w")
 local readme = io.open("README.md", "r")
 assert(index and readme, "failed to open files")
 
-index:write("---\nlayout: base\n---\n")
-index:write(readme:read("*a"))
+index:write("---\nlayout: home\ntitle: Vx\n---\n")
+local result, _ = readme:read("*a"):gsub("^[^\n]*\n", "")
+index:write(result)
 
 emit("Vx")
 emit("Event")
